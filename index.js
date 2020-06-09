ce0c8447e788707032988d3adf42d60986bd55c2
Vaibhav Goyal <vaibhav27072000@gmail.com>
2020-06-09 23:50:34 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImg1 = "dice" + randomNumber1 + ".png";
+var randomImg1Src = "images/" + randomImg1;
+
+document.querySelectorAll("img")[0].setAttribute("src" , randomImg1Src);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImg2 = "dice" + randomNumber2 + ".png";
+var randomImg2Src = "images/" + randomImg2;
+
+document.querySelectorAll("img")[1].setAttribute("src" , randomImg2Src);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
